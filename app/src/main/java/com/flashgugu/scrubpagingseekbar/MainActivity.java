f56ae09c2540f8c995568b6b16293b7b50b1c626
Jongchan Kim <flashilver@gmail.com>
2015-04-23 11:12:10 +0900

    add discreateseekbar

--- app/src/main/java/com/flashgugu/scrubpagingseekbar/MainActivity.java
+++ app/src/main/java/com/flashgugu/scrubpagingseekbar/MainActivity.java
@@ -2,16 +2,48 @@ package com.flashgugu.scrubpagingseekbar;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.SeekBar;
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static String TAG = "SeekBarTest";
+
+    DiscreteSeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (DiscreteSeekBar)findViewById(R.id.test_seek_bar);
+        seekBar.setHapticFeedbackEnabled(true);
+        seekBar.setMax(10);
+
+        seekBar.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
+            @Override
+            public void onProgressChanged(DiscreteSeekBar seekBar, int value, boolean fromUser) {
+                Log.d(TAG, Integer.toString(value));
+                Log.d(TAG, Float.toString(seekBar.getX()));
+                Log.d(TAG, Float.toString(seekBar.getY()));
+                Log.d(TAG, Boolean.toString(fromUser));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(DiscreteSeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(DiscreteSeekBar seekBar) {
+
+            }
+        });
+
     }
 
     @Override
---
+++ library/src/main/java/com/flashgugu/library/widgets/scrubpagingseekbar/ScrubPagingSeekBar.java
@@ -0,0 +1,23 @@
+package com.flashgugu.library.widgets.scrubpagingseekbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
+
+/**
+ * Created by jongchankim on 15. 4. 23.
+ */
+public class ScrubPagingSeekBar extends DiscreteSeekBar {
+    public ScrubPagingSeekBar(Context context) {
+        super(context);
+    }
+
+    public ScrubPagingSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrubPagingSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
